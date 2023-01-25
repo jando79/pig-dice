e921d4130fe0587651c7d8890c3cd50a68b4eb87
David Jandron <jandron1211@gmail.com>
2023-01-25 15:41:51 -0800

    ADD index, player 1 and 2 js

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,39 @@
 //Business Logic
 
+// Game Object Logic
 function Game () {
-  this.activePlayer = 1;
+  this.activePlayer = 0;
   this.gameActive = true;
-  this.players = []
+  this.players = [];
 }
 
-let game = new Game()
-let player 1 = new player()
-let player 2 = new player()
-game.players.push(player1)
-game.players.push(player2)
+Game.prototype.changePlayer = function() {
+  if (this.activePlayer === 0) {
+    this.activePlayer = 1;
+  } else {
+    this.activePlayer = 0;
+  }
+}
 
-game.players[0].overAllScore
-  array    
-game.players[0].rolldice()
+Game.prototype.endGame = function() {
+  if (this.player[0].overAllScore >= 100 || this.player[1].overAllScore >= 100) {
+    this.gameActive = false;
+  }
+}
+
+Game.prototype.newGame = function() {
+  this.activePlayer = 0;
+  this.players[0].currentScore = 0;
+  this.players[1].currentScore = 0;
+  this.players[0].overAllScore = 0;
+  this.players[1].overAllScore = 0;
+}
 
 function randomMath() {
   return Math.floor((Math.random() * 6) + 1);
 }
 
+// Player Object Logic
 function Player() {
   this.overAllScore = 0;
   this.turnScore = 0;
@@ -27,7 +41,6 @@ function Player() {
 
 Player.prototype.rollDice = function() {
   let roll = randomMath();
-  
   if (roll === 1) {
     this.turnScore = 0;
   } else {
@@ -39,3 +52,29 @@ Player.prototype.endTurn = function() {
   this.overAllScore += this.turnScore;
   this.turnScore = 0;
 }
+
+
+
+
+function handleFormSubmission(event) {
+  event.preventDefault();
+
+
+
+function handleRollButton() {
+  const rollButton = document.querySelector(".btn-roll");
+
+  let game = new Game()
+  let player1 = new Player();
+
+  
+}
+
+window.addEventListener("load", function (){
+  document.querySelector(".btn-new").addEventListener("click", NAMEOFNEWGAMEFUNCTION);
+
+  document.querySelector("btn-roll").addEventListener("click", NAMEOFROLLFUNCTION);
+
+  document.querySelector("btn-hold").addEventListener("click", NAMEOFHOLDFUNCTION);
+
+});
